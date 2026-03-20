2bee367bf2ef6137d0ebf691f0e6fdafe6c45a74
Andersonreports <andersonreportautomation@gmail.com>
2026-03-20 11:17:18 +0530

    supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -5,3 +5,10 @@ SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def upload_pdf(file_path, file_name):
+    with open(file_path, "rb") as f:
+        supabase.storage.from_("reports").upload(file_name, f)
+
+    file_url = f"{SUPABASE_URL}/storage/v1/object/public/reports/{file_name}"
+    return file_url
